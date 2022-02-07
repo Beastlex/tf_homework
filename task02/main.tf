a2503524a96b8d089de00fd2d658b3c5e04142dc
beastlex <beastlex@gmail.com>
2022-02-07 04:35:19 +0000

    2022-02-07: tf: task 02

---
+++ task02/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.91.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_virtual_network" "vnc" {
+  name = var.vnc_name
+  resource_group_name = var.rg_name
+}
+
+data "azurerm_subnet" "subnet" {
+  name = var.subnet_name
+  resource_group_name = var.rg_name
+  virtual_network_name = var.vnc_name
+}
+
+data "azurerm_network_security_group" "nsg" {
+  name = var.sg_name
+  resource_group_name = var.rg_name
+}
